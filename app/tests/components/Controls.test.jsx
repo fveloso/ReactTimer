65dd6d9152c409b7c37b60fc4b5a902c29388151
Fernando Jorge Veloso <Fernando.Veloso@wedotechnologies.com>
2016-12-09 11:49:16 +0000

    Finalize tests & add page title

--- app/tests/components/Controls.test.jsx
+++ app/tests/components/Controls.test.jsx
@@ -20,7 +20,7 @@ describe('Controls', () => {
         });
 
         it('should render start when started', () => {
-            var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="pause"/>);
+            var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="paused"/>);
             var $el = $(ReactDOM.findDOMNode(controls));
             var $startButton = $el.find('button:contains(Start)');
             expect($startButton.length).toBe(1);
